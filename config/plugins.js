2f70973feb12e99f58f99230542cc528a0800aca
pavo <kengzone@MacBook-Pro-3G.sctvnet.vn>
2023-06-19 13:09:40 +0700

    init backend

---
+++ config/plugins.js
@@ -0,0 +1,31 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    email: {
+      config: {
+        provider: 'sendgrid',
+        providerOptions: {
+          apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+          defaultFrom: 'ikavo799@gmail.com',
+          defaultReplyTo: 'ikavo799@gmail.com',
+        },
+      },
+    },
+    // ...
+  });
